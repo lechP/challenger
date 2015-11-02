e09a9e850977f526feec72dfe49f4f3395960011
LPI <cardano.p@gmail.com>
2015-11-02 12:35:47 +0100

    replaced Comparator with lambda expression

--- src/main/java/codility/magnesium/Pathfinder.java
+++ src/main/java/codility/magnesium/Pathfinder.java
@@ -2,29 +2,28 @@ package codility.magnesium;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class Pathfinder {
 
     public int solution(int N, int[] A, int[] B, int[] C) {
         int M = A.length;
-        List<Attraction> sortedAttractions = getSortedAttractions(C);
+        List<Integer> indexes = getSortedIndexes(C, M);
+
         int[] pathLengths = new int[N];
         List<Path> tempPaths = new ArrayList<>();
 
-        for (int i = 0; i < sortedAttractions.size(); i++) {
-            Attraction currentAttraction = sortedAttractions.get(i);
+        for (int i = 0; i < indexes.size(); i++) {
 
-            int nodeA = A[currentAttraction.index];
-            int nodeB = B[currentAttraction.index];
+            int nodeA = A[indexes.get(i)];
+            int nodeB = B[indexes.get(i)];
             int nodeAcurrentLength = pathLengths[nodeA];
             int nodeBcurrentLength = pathLengths[nodeB];
 
             tempPaths.add(new Path(nodeA, Math.max(nodeAcurrentLength, nodeBcurrentLength + 1)));
             tempPaths.add(new Path(nodeB, Math.max(nodeBcurrentLength, nodeAcurrentLength + 1)));
 
-            if (i == M - 1 || currentAttraction.value > sortedAttractions.get(i + 1).value) {
+            if (i == M - 1 || C[indexes.get(i)] > C[indexes.get(i+1)]) {
                 for (final Path tempPath : tempPaths) {
                     pathLengths[tempPath.startNode] = Math.max(pathLengths[tempPath.startNode], tempPath.length);
                 }
@@ -35,6 +34,15 @@ public class Pathfinder {
         return findMax(pathLengths);
     }
 
+    private List<Integer> getSortedIndexes(int[] C, int m) {
+        List<Integer> indexes = new ArrayList<>(m);
+        for (int i = 0; i < m; i++) {
+            indexes.add(i);
+        }
+        Collections.sort(indexes, (i, j) -> C[j] - C[i]);
+        return indexes;
+    }
+
     private int findMax(int[] pathLengths) {
         int max = 0;
         for (int pathLength : pathLengths) {
@@ -43,30 +51,6 @@ public class Pathfinder {
             }
         }
         return max;
-    }
-
-    private List<Attraction> getSortedAttractions(int[] C) {
-        List<Attraction> result = new ArrayList<>();
-        for (int i = 0; i < C.length; i++) {
-            result.add(new Attraction(i, C[i]));
-        }
-        Collections.sort(result, new Comparator<Attraction>() {
-            @Override
-            public int compare(Attraction o1, Attraction o2) {
-                return o2.value - o1.value;
-            }
-        });
-        return result;
-    }
-
-    private class Attraction {
-        int value;
-        int index;
-
-        Attraction(int index, int value) {
-            this.index = index;
-            this.value = value;
-        }
     }
 
     private class Path {
