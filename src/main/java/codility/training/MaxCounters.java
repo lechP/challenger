b6df7b596c626d4ff99b5300c9b0d5fb52191d35
LPI <cardano.p@gmail.com>
2015-11-03 20:51:02 +0100

    MaxCounters 100% solution

--- src/main/java/codility/training/MaxCounters.java
+++ src/main/java/codility/training/MaxCounters.java
@@ -1,27 +1,22 @@
 package codility.training;
 
-/**
- * TODO LPI comment me
- *
- * @author Lech Piechota, Capgemini
- */
 public class MaxCounters {
 
-    public int[] solution(int N, int[] A){
-        int [] result = new int[N];
+    public int[] solution(int N, int[] A) {
+        int[] result = new int[N];
 
-        int localMax =0;
+        int localMax = 0;
         int globalMax = 0;
         int lastGlobal = -1;
 
         for (int i = 0; i < A.length; i++) {
-            if(A[i] > N){
-                globalMax += localMax;
+            if (A[i] > N) {
+                globalMax = localMax;
                 localMax = 0;
                 lastGlobal = i;
             } else {
-                if(lastGlobal + 1 == i) {
-                    result = new int[N];
+                if (result[A[i] - 1] < globalMax) {
+                    result[A[i] - 1] = globalMax;
                 }
                 result[A[i] - 1]++;
                 if (localMax < result[A[i] - 1]) {
@@ -29,6 +24,13 @@ public class MaxCounters {
                 }
             }
         }
+
+        result = new int[N];
+
+        for (int i = lastGlobal + 1; i < A.length; i++) {
+            result[A[i] - 1]++;
+        }
+
         for (int i = 0; i < result.length; i++) {
             result[i] += globalMax;
 
